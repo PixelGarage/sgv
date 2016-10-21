3f55859e8b8b470b8ec6e225be86d89d0d495743
Ralph Moser <ralph@ramosoft.ch>
2016-10-21 23:49:59 +0200

    - user import performed - styling continued - floating anchor menu implemented - db updated

--- all/themes/pixelgarage/js/pxlBehaviors.js
+++ all/themes/pixelgarage/js/pxlBehaviors.js
@@ -24,32 +24,6 @@
   };
 
   /**
-   * Anchor menus: Scrolls smoothly to anchor due to menu click.
-   */
-  Drupal.behaviors.smoothScrolltoAnchors = {
-    attach: function(context, settings) {
-      $(function() {
-        $('.menu li.leaf a').click(function() {
-          var anchorPos = this.href.indexOf('#');
-          // no anchor available, perform click
-          if (anchorPos == -1) return true;
-
-          // menu item references anchor, get anchor target
-          var target = $(this.href.slice(pos));
-          if (target.length) {
-            $('html, body').stop().animate({
-              scrollTop: target.offset().top
-            }, 1000, 'swing');
-            return false;
-          }
-          // no target available, perform click
-          return true;
-        });
-      });
-    }
-  };
-
-  /**
    * Allows full size clickable items.
    */
    Drupal.behaviors.fullSizeClickableItems = {
@@ -60,6 +34,79 @@
         $(this).on('click', function () {
           window.location = $(this).find("a:first").attr("href");
           return false;
+        });
+      });
+    }
+  };
+
+  /**
+   * Add a floating paragraphs anchor menu to the body and position it
+   * according to the scroll position.
+   */
+  Drupal.behaviors.createParagraphAnchorMenu = {
+    attach: function () {
+      // add anchor menu container
+      var $mainContainer = $('.main-container');
+
+      $mainContainer.prepend('<div class="menu-anchor-container"><ul class="menu-anchor"></ul></div>');
+
+      var $anchorMenu = $mainContainer.find('.menu-anchor'),
+        $anchorMenuCont = $mainContainer.find('.menu-anchor-container'),
+        $paragraphItems = $('#block-system-main').find('.entity-paragraphs-item');
+
+      // add anchor menu for each paragraph
+      $anchorMenuCont.hide();
+      $paragraphItems.each(function(index) {
+        var $item = $(this),
+            anchor = 'paragraph-item-' + index,
+            anchorTitle = $item.attr('data-anchor-title') ? $item.attr('data-anchor-title') : false;
+
+        // add anchor (id) to paragraph
+        $item.attr('id', anchor);
+
+        // create anchor menu for paragraph
+        if (anchorTitle) {
+          $anchorMenu.append('<li class="leaf"><a href="#' + anchor + '">' + anchorTitle + '</a></li>');
+          $anchorMenuCont.show();
+        }
+      });
+
+      //
+      // position anchor menu during scrolling
+      if ($anchorMenuCont.is(':visible')) {
+        $(window).on('scroll', function() {
+          var scrollPos = $(window).scrollTop();
+
+          $anchorMenuCont.css({'top': scrollPos});
+        });
+      }
+    }
+  };
+
+  /**
+   * Anchor menus: Scrolls smoothly to anchor due to menu click.
+   */
+  Drupal.behaviors.smoothScrolltoAnchors = {
+    attach: function(context, settings) {
+      $(function() {
+        $('.menu-anchor li.leaf a').click(function() {
+          var anchorPos = this.href.indexOf('#'),
+            $mainContainer = $('.main-container'),
+            mainTop = $mainContainer.offset().top;
+
+          // no anchor available, perform click
+          if (anchorPos == -1) return true;
+
+          // menu item references anchor, get anchor target
+          var target = $(this.href.slice(anchorPos));
+          if (target.length) {
+            $('html, body').stop().animate({
+              scrollTop: target.offset().top - mainTop
+            }, 800, 'swing');
+            return false;
+          }
+          // no target available, perform click
+          return true;
         });
       });
     }
