f8e3e6286ad5b507436d7991b725b25fe87ba359
Ralph Moser <ralph@ramosoft.ch>
2016-11-01 21:22:21 +0100

    - styling first phase finished - db updated

--- all/themes/pixelgarage/templates/views-view-unformatted--documents.tpl.php
+++ all/themes/pixelgarage/templates/views-view-unformatted--documents.tpl.php
@@ -6,6 +6,15 @@
  *
  * @ingroup views_templates
  */
+
+//
+// get extensions of linked documents
+$array_document_ext = array();
+foreach ($view->result as $index => $document) {
+  $filename = $document->field_field_document[0]['raw']['filename'];
+  $array_document_ext[$index] = strtoupper(pathinfo($filename, PATHINFO_EXTENSION));
+}
+
 ?>
 <?php if (!empty($title)): ?>
   <div class="column-lang"><h3 class="column-title"><?php print $title; ?></h3>
@@ -16,6 +25,7 @@
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
+    <div class="document-ext"><?php print $array_document_ext[$id]; ?></div>
   </div>
 <?php endforeach; ?>
 
